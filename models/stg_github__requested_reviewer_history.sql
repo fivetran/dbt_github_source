e75717bfd73a9b32686775e807a93481deb6b764
fivetran-joemarkiewicz <joseph.markiewicz@fivetran.com>
2020-11-23 15:12:20 -0800

    final feature/updates commit

--- models/stg_github__requested_reviewer_history.sql
+++ models/stg_github__requested_reviewer_history.sql
@@ -5,12 +5,21 @@ with requested_reviewer_history as (
 
 ), macro as (
     select
+        /*
+        The below macro is used to generate the correct SQL for package staging models. It takes a list of columns 
+        that are expected/needed (staging_columns from dbt_github_source/models/tmp/) and compares it with columns 
+        in the source (source_columns from dbt_github_source/macros/).
+
+        For more information refer to our dbt_fivetran_utils documentation (https://github.com/fivetran/dbt_fivetran_utils.git).
+        */
             {{
             fivetran_utils.fill_staging_columns(
                 source_columns=adapter.get_columns_in_relation(ref('stg_github__requested_reviewer_history_tmp')),
                 staging_columns=get_requested_reviewer_history_columns()
             )
         }}
+
+        --The below script allows for pass through columns.
         {% if var('requested_reviewer_history_pass_through_columns') != [] %}
         ,
         {{ var('requested_reviewer_history_pass_through_columns') | join (", ")}}
--- models/stg_github__user.sql
+++ models/stg_github__user.sql
@@ -5,12 +5,21 @@ with github_user as (
 
 ), macro as (
     select
+        /*
+        The below macro is used to generate the correct SQL for package staging models. It takes a list of columns 
+        that are expected/needed (staging_columns from dbt_github_source/models/tmp/) and compares it with columns 
+        in the source (source_columns from dbt_github_source/macros/).
+
+        For more information refer to our dbt_fivetran_utils documentation (https://github.com/fivetran/dbt_fivetran_utils.git).
+        */
             {{
             fivetran_utils.fill_staging_columns(
                 source_columns=adapter.get_columns_in_relation(ref('stg_github__user_tmp')),
                 staging_columns=get_user_columns()
             )
         }}
+
+        --The below script allows for pass through columns.
         {% if var('user_pass_through_columns') != [] %}
         ,
         {{ var('user_pass_through_columns') | join (", ")}}
